1cfb35b4a6253b113d54c6c083c114b89cfb5f08
JamuDuck <122187056+JamuDuck@users.noreply.github.com>
2024-12-04 21:49:08 -0600

    void func

---
+++ code.py
@@ -0,0 +1,9 @@
+import random
+
+# Void Function
+def showInstructions():
+    print("Welcome to the Space Adventure Game!")
+    print("Your goal is to travel from Neptune to Earth by answering questions.")
+    print("Each time you answer incorrectly, you lose 20 HP and may meet an alien.")
+    print("If you encounter a friendly alien, your HP increases. A bad alien decreases it.")
+    print("If your HP reaches 0, you lose. If you answer all questions correctly, you win!")
